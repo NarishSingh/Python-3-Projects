9cdb5f8dc70691f1873064b376c380aef0a5ff2a
Narish Singh <narish.singh786@gmail.com>
2020-04-21 22:14:36 -0400

    Edited header, and now exits on faulty inputs

--- GrossPayCalc/GrossPay.py
+++ GrossPayCalc/GrossPay.py
@@ -1,17 +1,20 @@
-# Write a program to prompt the user for hours and rate per hour to compute gross pay.
-# give the employee 1.5 times the hourly rate for hours worked above 40 hours.
-# use try and except so that your program handles non-numeric input gracefully.
+# Author: Narish Singh
+# Date Created: 4/19
+# Last Modified: 4/19
+import sys
 
 workHrMax = 40
 try:
     hr = float(input("Enter hours worked this week: "))
 except ValueError:
-    print("Please enter a numerical value.")
+    print("Not a numerical value.")
+    sys.exit()
 
 try:
     hrPay = round(float(input("Enter hourly pay rate: $")), 2)
 except ValueError:
-    print("Please enter a numerical value.")
+    print("Not a numerical value.")
+    sys.exit()
 
 if hr > 40:
     overtime = hr - 40
