40c69f877929047adab70056ebe0b03e9e6343ba
Narish Singh <narish.singh786@gmail.com>
2020-05-13 18:20:41 -0400

    Added documentation for functions and some error handling

--- distanceconverter/DistanceConversion.py
+++ distanceconverter/DistanceConversion.py
@@ -6,6 +6,10 @@ import sys
 
 
 def display_unit_list():
+    """
+    Display unit list for the menu
+    :return: {void}
+    """
     print("===Customary===")
     print("1 | Inch")
     print("2 | Foot")
@@ -19,6 +23,10 @@ def display_unit_list():
 
 
 def get_user_menu_choice():
+    """
+    get user input for menu, constrains input using while loop
+    :return: {int} 1-8 for the desired unit of measure throws ValueError exception if not number
+    """
     menu_input = 0
 
     while menu_input < 1 or menu_input > 8:
@@ -32,6 +40,11 @@ def get_user_menu_choice():
 
 
 def menu_choice_as_string(menu_input):
+    """
+    Convert numerical input for menu selection to String
+    :param menu_input: {int} 1-8 corresponding to menu inputs, exits program otherwise
+    :return:
+    """
     if menu_input == 1:
         return "in"
     elif menu_input == 2:
@@ -48,9 +61,18 @@ def menu_choice_as_string(menu_input):
         return "m"
     elif menu_input == 8:
         return "km"
+    else:
+        print("Unknown command")
+        sys.exit()
 
 
 def process_distance(from_unit, distance):
+    """
+    Process starting measure into feet or meters based on whether it was originally customary or metric
+    :param from_unit: {int} 1-8 corresponding to menu inputs, exits program otherwise
+    :param distance: {float} distance to be processed
+    :return:
+    """
     if from_unit == 1:
         return distance / 12
     elif from_unit == 3:
@@ -69,6 +91,13 @@ def process_distance(from_unit, distance):
 
 
 def convert_distance(to_unit, dist_in_ft, dist_in_m):
+    """
+    convert distance from feet or meters to another unit of measure
+    :param to_unit: {int} 1-8 corresponding to menu inputs, exits program otherwise
+    :param dist_in_ft: {float} user's distance in feet
+    :param dist_in_m: {float} user's distance in meters
+    :return: {float} distance converted from ft/m to intended unit of measure
+    """
     if to_unit == 1:
         return dist_in_ft * 12
     elif to_unit == 2:
@@ -90,7 +119,14 @@ def convert_distance(to_unit, dist_in_ft, dist_in_m):
         sys.exit()
 
 
-def conversion(from_unit, to_unit, distance):  # fill in conversions
+def conversion(from_unit, to_unit, distance):
+    """
+    controller for converting distance from one unit of measure to another
+    :param from_unit: {int} 1-8 corresponding to menu inputs
+    :param to_unit: {int} 1-8 corresponding to menu inputs
+    :param distance: {float} distance to be processed
+    :return: {float} distance converted to intended unit of measure
+    """
     if from_unit == to_unit:
         print("No conversion to be done")
         sys.exit()
@@ -100,15 +136,22 @@ def conversion(from_unit, to_unit, distance):  # fill in conversions
 
         converted = convert_distance(to_unit, dist_in_ft, dist_in_m)
         return converted
-    else:
+    elif from_unit == 5 or from_unit == 6 or from_unit == 7 or from_unit == 8:
         dist_in_m = process_distance(from_unit, distance)
         dist_in_ft = dist_in_m * 3.28
 
         converted = convert_distance(to_unit, dist_in_ft, dist_in_m)
         return converted
+    else:
+        print("Unknown command")
+        sys.exit()
 
 
 def main():
+    """
+    app controller
+    :return: {void}
+    """
     print("Welcome to the distance converter")
 
     print("Please select the starting unit: ")
@@ -119,7 +162,7 @@ def main():
     ending_unit = get_user_menu_choice()
 
     try:
-        dist = int(input("Please enter distance: "))
+        dist = float(input("Please enter distance: "))
     except ValueError:
         print("Not a numerical input")
         sys.exit()
