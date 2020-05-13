fa4b04cfe3ce05c06be4c91d88e34ba92826b338
Narish Singh <narish.singh786@gmail.com>
2020-05-13 15:10:09 -0400

    program complete, name changes to match convention

--- distanceconverter/DistanceConversion.py
+++ distanceconverter/DistanceConversion.py
@@ -5,7 +5,7 @@
 import sys
 
 
-def displayUnitList():
+def display_unit_list():
     print("===Customary===")
     print("1 | Inch")
     print("2 | Foot")
@@ -18,109 +18,105 @@ def displayUnitList():
     print("8 | Kilometer")
 
 
-def getUserMenuChoice():
-    menuInput = 0
+def get_user_menu_choice():
+    menu_input = 0
 
-    while menuInput < 1 or menuInput > 8:
+    while menu_input < 1 or menu_input > 8:
         try:
-            menuInput = int(input("Convert from: "))
+            menu_input = int(input("Unit: "))
         except ValueError:
             print("Not a numerical input")
             sys.exit()
 
-    return menuInput
+    return menu_input
 
 
-# convert all to ft or m
-def inToFt(inches):
-    return inches / 12
+def menu_choice_as_string(menu_input):
+    if menu_input == 1:
+        return "in"
+    elif menu_input == 2:
+        return "ft"
+    elif menu_input == 3:
+        return "yd"
+    elif menu_input == 4:
+        return "mi"
+    elif menu_input == 5:
+        return "mm"
+    elif menu_input == 6:
+        return "cm"
+    elif menu_input == 7:
+        return "m"
+    elif menu_input == 8:
+        return "km"
 
 
-def ydToFt(yd):
-    return yd * 3
+def process_distance(from_unit, distance):
+    if from_unit == 1:
+        return distance / 12
+    elif from_unit == 3:
+        return distance * 3
+    elif from_unit == 4:
+        return distance * 5280
+    elif from_unit == 5:
+        return distance / 1000
+    elif from_unit == 6:
+        return distance / 100
+    elif from_unit == 8:
+        return distance * 1000
+    else:
+        print("Unknown command")
+        sys.exit()
 
 
-def miToFt(mi):
-    return mi * 5280
+def convert_distance(to_unit, dist_in_ft, dist_in_m):
+    if to_unit == 1:
+        return dist_in_ft * 12
+    elif to_unit == 2:
+        return dist_in_ft
+    elif to_unit == 3:
+        return dist_in_ft / 3
+    elif to_unit == 4:
+        return dist_in_ft / 5280
+    elif to_unit == 5:
+        return dist_in_m * 1000
+    elif to_unit == 6:
+        return dist_in_m * 100
+    elif to_unit == 7:
+        return dist_in_m
+    elif to_unit == 8:
+        return dist_in_m / 1000
+    else:
+        print("Unknown command")
+        sys.exit()
 
 
-def mmToM(mm):
-    return mm / 1000
-
-
-def cmToM(cm):
-    return cm / 100
-
-
-def kmToM(km):
-    return km * 1000
-
-
-def processDistance(fromUnit, distance):
-    # process customary and metric measures
-    if fromUnit == 1:
-        return inToFt(distance)
-    elif fromUnit == 3:
-        return ydToFt(distance)
-    elif fromUnit == 4:
-        return miToFt(distance)
-    elif fromUnit == 5:
-        return mmToM(distance)
-    elif fromUnit == 6:
-        return cmToM(distance)
-    elif fromUnit == 8:
-        return kmToM(distance)
-
-
-def convert(fromUnit, toUnit, distance):  # fill in conversions
-    if fromUnit == toUnit
+def conversion(from_unit, to_unit, distance):  # fill in conversions
+    if from_unit == to_unit:
         print("No conversion to be done")
         sys.exit()
-    elif fromUnit == 1 or fromUnit == 2 or fromUnit == 3 or fromUnit == 4:
-        distInFt = processDistance(fromUnit, distance)
-        distInM = distInFt / 3.28
+    elif from_unit == 1 or from_unit == 2 or from_unit == 3 or from_unit == 4:
+        dist_in_ft = process_distance(from_unit, distance)
+        dist_in_m = dist_in_ft / 3.28
 
-        if toUnit == 1:
-            return distInFt * 12
-        elif toUnit == 2:
-            return distInFt
-        elif toUnit == 3:
-            return distInFt / 3
-        elif toUnit == 4:
-            return distInFt / 5280
-        elif toUnit == 5:
-            return distInM * 1000
-        elif toUnit == 6:
-            return distInM * 100
-        elif toUnit == 7:
-            return distInM
-        elif toUnit == 8:
-            return distInM / 1000
+        converted = convert_distance(to_unit, dist_in_ft, dist_in_m)
+        return converted
     else:
-        distInM = processDistance(fromUnit, distance)
-        distInFt = distInM * 3.28
+        dist_in_m = process_distance(from_unit, distance)
+        dist_in_ft = dist_in_m * 3.28
 
-        # TODO finish conversions
-        if toUnit == 1:
-        elif toUnit == 2:
-        elif toUnit == 3:
-        elif toUnit == 4:
-        elif toUnit == 5:
-        elif toUnit == 6:
-        elif toUnit == 7:
-        elif toUnit == 8:
+        converted = convert_distance(to_unit, dist_in_ft, dist_in_m)
+        return converted
 
 
 def main():
     print("Welcome to the distance converter")
 
     print("Please select the starting unit: ")
-    displayUnitList()
-    startingUnit = getUserMenuChoice()
+    display_unit_list()
+    starting_unit = get_user_menu_choice()
 
     print("Select unit to convert to")
-    displayUnitList()
-    endingUnit = getUserMenuChoice()
+    ending_unit = get_user_menu_choice()
 
     try:
         dist = int(input("Please enter distance: "))
@@ -128,4 +124,11 @@ def main():
         print("Not a numerical input")
         sys.exit()
 
-    print(convert(startingUnit, endingUnit, dist))
+    start_unit_string = menu_choice_as_string(starting_unit)
+    end_unit_string = menu_choice_as_string(ending_unit)
+    converted_distance = conversion(starting_unit, ending_unit, dist)
+    print(dist, start_unit_string, " = ", format(converted_distance, ',.4f'),
+          end_unit_string)
+
+
+main()
