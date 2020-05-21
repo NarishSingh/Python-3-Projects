fa0a5e8f724ceaba0d40458e3159b3b8e980ce24
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-05-21 12:45:15 -0400

    updated terminology of variables to better match casino gameplay

--- luckysevensgame/lucky7.py
+++ luckysevensgame/lucky7.py
@@ -1,6 +1,6 @@
 # Lucky Sevens (Game version)
 # Date Created: 5/20/20
-# Last Modified: 5/20/20
+# Last Modified: 5/21/20
 
 import random
 
@@ -16,65 +16,65 @@ def roll_2_dice():
     return random.randint(2, 13)
 
 
-def evaluate_round(roll, bet, pot):
+def evaluate_round(roll, pot, bank):
     """
     Determine win or loss and award double the bet for win, or take bet from pot for loss
     :param roll: {int} 2-12 from rolling 2 dice
-    :param bet: {float} player's round bet
-    :param pot: {float} player's pot
+    :param pot: {float} player's round bet
+    :param bank: {float} player's total pot
     :return: {float} the player's pot after round
     """
     if roll == 7:
-        pot += (2 * bet)
+        bank += (2 * pot)
     else:
-        pot -= bet
+        bank -= pot
 
-    return pot
+    return bank
 
 
-def track_best(round_ct, pot):
+def track_best(round_ct, bank):
     """
     Track which round player earned the highest amount
     :param round_ct: {int} round number
-    :param pot: {float} current earning
+    :param bank: {float} current earning
     """
     global best_earning
     global best_round
 
-    if pot > best_earning:
-        best_earning = pot
+    if bank > best_earning:
+        best_earning = bank
         best_round = round_ct
 
 
-def play_rounds(pot):
+def play_rounds(bank):
     """
     Main gameplay loop while player has money in their pot
-    :param pot: {float} player's money
+    :param bank: {float} player's money
     """
     playing = True
     round_ct = 1
 
-    while pot > 0 and playing:
+    while bank > 0 and playing:
         print("Round", round_ct)
-        bet = round(float(input("Place your bet (0 for all-in): ")), 2)
-        while bet > pot:
+        pot = round(float(input("Place your pot (0 for all-in): ")), 2)
+        while pot > bank:
             print("Bet cannot be bigger than your pot.")
-            bet = round(float(input("Place your bet: $")), 2)
+            pot = round(float(input("Place your pot: $")), 2)
 
-        if bet == 0 or bet == pot:
-            bet = pot
+        if pot == 0 or pot == bank:
+            pot = bank
             print("All-in...")
 
         round_roll = roll_2_dice()
         print("You rolled", round_roll)
 
-        pot = evaluate_round(round_roll, bet, pot)
-        print("Pot: $", format(pot, ',.2f'), sep='')
+        bank = evaluate_round(round_roll, pot, bank)
+        print("Pot: $", format(bank, ',.2f'), sep='')
 
-        track_best(round_ct, pot)
+        track_best(round_ct, bank)
         round_ct += 1
 
-        if pot > 0:
+        if bank > 0:
             new_round = int(input("Play another round? (1 = yes | 2 = no): "))
             if new_round == 1:
                 playing = True
@@ -93,12 +93,12 @@ def main():
     best_earning = 0
 
     print("Welcome to Lucky Sevens")
-    pot = round(float(input("Enter buy-in: $")), 2)
-    while pot < 0:
+    bank = round(float(input("Enter buy-in: $")), 2)
+    while bank < 0:
         print("Invalid buy-in")
-        pot = round(float(input("Enter buy-in: $")), 2)
+        bank = round(float(input("Enter buy-in: $")), 2)
 
-    play_rounds(pot)
+    play_rounds(bank)
 
     print("You peaked at round ", best_round, " with a maximum earning of $", format(best_earning, ",.2f"), sep='')
 
