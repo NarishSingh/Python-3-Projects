1369eaa7e6260f4429ca7f27f62c2e655bd881a0
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-05-23 21:35:00 -0400

    Added functionality to print all prime numbers up to user input

--- primalitytest/primality.py
+++ primalitytest/primality.py
@@ -18,22 +18,50 @@ def is_prime(num):
     return True
 
 
+def prime_list(num):
+    """
+    Test for primes up to the argument and print them
+    :param num: int - positive integer above 1
+    """
+    for i in range(2, num + 1):
+        if is_prime(i):
+            print(i, end=', ')
+
+    print()
+
+
 def main():
-    calculating = True
-
     print("===Prime Number Test===")
+    print("1 | Calculate for a Prime")
+    print("2 | Print all Primes")
+    menu = int(input("Action: "))
 
-    while calculating:
-        num = int(input("Enter a integer (0 or 1 to exit): "))
+    if menu == 1:
+        calculating = True
+        while calculating:
+            num = int(input("Enter a integer (0 or 1 to exit): "))
 
-        if num == 0 or num == 1:
-            calculating = False
-        elif num < 0:
-            print("Cannot solve for a negative number")
-        elif is_prime(num):
-            print(num, "is a prime number!")
-        else:
-            print(num, "is not a prime number...")
+            if num == 0 or num == 1:
+                calculating = False
+            elif num < 0:
+                print("Cannot solve for a negative number")
+            elif is_prime(num):
+                print(num, "is a prime number!")
+            else:
+                print(num, "is not a prime number...")
+    elif menu == 2:
+        listing = True
+
+        while listing:
+            test_to = int(input("Enter a integer (0 or 1 to exit): "))
+
+            if test_to == 0 or test_to == 1 or test_to < 0:
+                print("No primes in range")
+                listing = False
+            else:
+                print(prime_list(test_to))
+    else:
+        print("Unknown command...")
 
 
 main()
