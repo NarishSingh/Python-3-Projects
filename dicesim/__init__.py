da758586192cad8fbe2355711a20768a876f2736
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-12-16 14:33:13 -0500

    dice roll sim

---
+++ dicesim/__init__.py
@@ -0,0 +1,29 @@
+import random as rand
+
+MIN = 1
+MAX = 6
+
+
+def roll(numDice: int) -> int:
+    """
+    Roll any number of dice once
+    :param numDice: {int} number of dice to roll
+    :return: roll result
+    """
+    return rand.randint(MIN * numDice, MAX * numDice)
+
+
+def main() -> None:
+    rolls = []
+
+    numDice = int(input("Enter number of dice: "))
+    numRolls = int(input("Enter number of rolls: "))
+
+    for n in range(numRolls):
+        rolls.append(roll(numDice))
+
+    for r in rolls:
+        print(r, end=' ')
+
+
+main()
